1f239f95dc75d6dfabc14b06e3d7a459749ad021
lpascarelli <luca.pascarelli@brocc.se>
2024-03-03 17:59:06 +0100

    learning resource app

---
+++ learning-resource-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <learning-resource v-for="resource in storedResources" :key="resource.id" :title="resource.title" :description="resource.description" :link="resource.link" />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: '2',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com'
+        },
+      ]
+    }
+  }
+}
+</script>
