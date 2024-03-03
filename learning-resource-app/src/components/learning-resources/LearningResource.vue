e5748956e13c2acf2ac136ac8cbff7f7e3ee1f70
lpascarelli <luca.pascarelli@brocc.se>
2024-03-03 18:11:32 +0100

    Styling and creating new components

--- learning-resource-app/src/components/learning-resources/LearningResource.vue
+++ learning-resource-app/src/components/learning-resources/LearningResource.vue
@@ -10,7 +10,7 @@
       {{ description }}
     </p>
     <nav>
-      <a :href="link">Vue resource</a>
+      <a :href="link">View resources</a>
     </nav>
   </li>
 </template>
@@ -20,3 +20,35 @@
     props: ['title', 'description', 'link'],
   }
 </script>
+
+<style scoped>
+  li {
+    margin: auto;
+    max-width: 40rem;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+  }
+
+  p {
+    margin: 0.5rem 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: #ce5c00;
+  }
+
+  a:hover,
+  a:active {
+    color: #c89300;
+  }
+</style>
