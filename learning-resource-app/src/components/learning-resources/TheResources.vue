bc5a93578e56c178ceb1e684152784980c904dda
lpascarelli <luca.pascarelli@brocc.se>
2024-03-05 15:27:20 +0100

    Add new resources

--- learning-resource-app/src/components/learning-resources/TheResources.vue
+++ learning-resource-app/src/components/learning-resources/TheResources.vue
@@ -3,7 +3,9 @@
     <base-button :mode="storedResButtonMode" @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
     <base-button :mode="addResButtonMode" @click="setSelectedTab('add-resource')">Add Resource</base-button>
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -45,11 +47,23 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
     }
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title,
+        description,
+        link,
+      }
+
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     }
   }
 }
